8dc5e54dc83fac14c00333de103cf35ed77b17cb
Thiago <Thiagodacostaalves35@gmail.com>
2026-02-01 21:18:03 -0300

    feat: Add `sessionService` dependency to `AuthService`.

--- backend/src/features/Auth/index.ts
+++ backend/src/features/Auth/index.ts
@@ -2,8 +2,9 @@ import { AuthService } from "./AuthService.js";
 import { AuthController } from "./AuthController.js";
 import { userService } from "../User/index.js";
 import { companyService } from "../Company/index.js";
+import { sessionService } from "../Session/index.js";
 
-const authService = new AuthService(userService, companyService);
+const authService = new AuthService(userService, companyService, sessionService);
 const authController = new AuthController(authService);
 
 export { authService, authController };
