f6f9a82ebe7934bf6d2ac9fa08dc62a1a78d1e83
Thiago <Thiagodacostaalves35@gmail.com>
2026-02-06 16:41:41 -0300

    feat: add swagger-jsdoc dependency and define Sale repository interfaces

--- backend/src/features/Sale/repositories/ISaleRepository.ts
+++ backend/src/features/Sale/repositories/ISaleRepository.ts
@@ -1,1 +1,17 @@
-export interface
+import { Sale, SaleStatus } from "@prisma/client";
+import { IRepository } from "../../../core/interface/IRepository.js";
+
+export interface IFindByDateRangeInput {
+    start: Date;
+    end: Date;
+    companyId: string;
+}
+
+export interface IFindSaleByStatusInput {
+    status: SaleStatus;
+}
+
+export interface ISaleRepository extends IRepository<Sale, Partial<Sale>> {
+    findByDateRange(findByDateRangeInput: IFindByDateRangeInput): Promise<Sale>;
+    findByStatus(findByStatus: IFindSaleByStatusInput): Promise<Sale[]>;
+}
